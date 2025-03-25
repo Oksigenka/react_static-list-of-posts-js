5d5db7f6390cdbbab68fafb7ec2c0151b30b2c2f
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2025-03-25 21:31:59 +0100

    Posts task

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,18 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+
+import { PostInfo } from "../PostInfo";
+
+export const PostList = ({ posts }) => {
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo
+          key={post.id}
+          post={post}
+          user={post.user}
+          comments={post.comments}
+        />
+      ))}
+    </div>
+  );
+};
